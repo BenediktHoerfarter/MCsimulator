0ed6c55906511493c58bbb60faafb70e8ac21d30
BenediktHoerfarter <benedikt.hoerfarter@gmail.com>
2025-09-22 20:58:24 +0200

    Hotfix: Correct double-counting in ljEnergyCalculator()

--- MCsimulator/utils/utils.py
+++ MCsimulator/utils/utils.py
@@ -4,8 +4,10 @@ def ljEnergy(Alj: float, Blj: float, distance: float) -> float:
 
 def ljEnergyCalculator(atomsWithCoordinatesList: list, parameterString: str, vdWCutoff: float) -> float:
     EpotTotal: float = 0.0
+    numAtoms: int = len(atomsWithCoordinatesList)
 
-    for atomA in atomsWithCoordinatesList:
+    for i in range(numAtoms - 1):
+        atomA = atomsWithCoordinatesList[i]
         atomAx: float = atomA[1]
         atomAy: float = atomA[2]
         atomAz: float = atomA[3]
@@ -14,28 +16,22 @@ def ljEnergyCalculator(atomsWithCoordinatesList: list, parameterString: str, vdW
         AljPosStart: int = parameterString.find("=", AljPos)
         AljPosEnd: int = parameterString.find(";", AljPos)
         Alj: float = float(parameterString[AljPosStart + 1 : AljPosEnd].strip())
-        for atomB in atomsWithCoordinatesList:
-            if atomB == atomA:
-                continue
-            else:
-                atomBx: float = atomB[1]
-                atomBy: float = atomB[2]
-                atomBz: float = atomB[3]
-                squareDistance: float = (
-                    (atomAx - atomBx) ** 2
-                    + (atomAy - atomBy) ** 2
-                    + (atomAz - atomBz) ** 2
-                )
-                if squareDistance <= vdWCutoff**2:
-                    atomBPos: int = parameterString.find(atomB[0] + ":", atomAPos)
-                    BljPos: int = parameterString.find("B_lj", atomBPos)
-                    BljPosStart: int = parameterString.find("=", BljPos)
-                    BljPosEnd: int = parameterString.find(";", BljPos)
-                    Blj: float = float(
-                        parameterString[BljPosStart + 1 : BljPosEnd].strip()
-                    )
-                    distance: float = squareDistance ** (1 / 2)
-                    EpotTotal: float = EpotTotal + ljEnergy(Alj, Blj, distance)
+        for j in range(i + 1, numAtoms):
+            atomB = atomsWithCoordinatesList[j]
+            atomBx: float = atomB[1]
+            atomBy: float = atomB[2]
+            atomBz: float = atomB[3]
+            squareDistance: float = (
+                (atomAx - atomBx) ** 2 + (atomAy - atomBy) ** 2 + (atomAz - atomBz) ** 2
+            )
+            if squareDistance <= vdWCutoff**2:
+                atomBPos: int = parameterString.find(atomB[0] + ":", atomAPos)
+                BljPos: int = parameterString.find("B_lj", atomBPos)
+                BljPosStart: int = parameterString.find("=", BljPos)
+                BljPosEnd: int = parameterString.find(";", BljPos)
+                Blj: float = float(parameterString[BljPosStart + 1 : BljPosEnd].strip())
+                distance: float = squareDistance ** (1 / 2)
+                EpotTotal: float = EpotTotal + ljEnergy(Alj, Blj, distance)
     return EpotTotal
 
 
